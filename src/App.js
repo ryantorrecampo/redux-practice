50bcc3f7db828f68b13c92e60d8b50adadc23494
ryantorecampo <ryan.torrecampo27@gmail.com>
2019-09-05 18:11:41 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, signIn, reset } from "./actions";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const counter = useSelector(state => state.counter);
+    const isLogged = useSelector(state => state.isLogged);
+    const dispatch = useDispatch();
+    return (
+        <div className="App">
+            <h1>Counter {counter}</h1>
+            <button onClick={() => dispatch(increment(5))}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(reset())}>Reset Counter</button>
+            <button onClick={() => dispatch(signIn())}>Sign In</button>
+            {isLogged ? <h3>Valueable Information</h3> : ""}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+export const increment = nr => {
+    return {
+        type: "INCREMENT",
+        payload: nr
+    };
+};
+
+export const decrement = () => {
+    return {
+        type: "DECREMENT"
+    };
+};
+
+export const signIn = () => {
+    return {
+        type: "SIGN_IN"
+    };
+};
+
+export const reset = () => {
+    return {
+        type: "RESET"
+    };
+};
